15dbfd37a09f43cc88c9ebddd6ce33b5d0c9c625
natprz42 <natprz42@gmail.com>
2024-06-30 14:00:24 +0200

    Add Expense logic

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import Income from '../src/components/Income';
+import Expense from '../src/components/Expense'
+import Navbar from "./components/Navbar";
 
 function App() {
+    const [expenses, setExpenses] = useState([]);
     const [incomes, setIncomes] = useState([]);
     const [totalIncome, setTotalIncome] = useState(0);
     const [totalExpenditure, setTotalExpenditure] = useState(0);
     const [remainingIncome, setRemainingIncome] = useState(0);
+    const [percentageExpended, setPercentageExpended] = useState(0);
 
     // Calculate total income
     useEffect(() => {
@@ -18,22 +22,44 @@ function App() {
         );
         setTotalIncome(total);
 
+            total = 0;
+            expenses.forEach(
+                (expense) =>
+                    (total += expense.amount)
+            );
+            setTotalExpenditure(total);
+
         // Calculate remaining income
         setRemainingIncome(totalIncome - totalExpenditure);
+
+// Calculate percentage expended
+        if (totalIncome !== 0) {
+            setPercentageExpended(
+                (totalExpenditure / totalIncome) * 100
+            );
+        }
+    }, [incomes, expenses, totalIncome, totalExpenditure]);
+
+    const addExpense = (expense) => {
+        setExpenses([...expenses, expense]);
+    };
 
     const addIncome = (income) => {
         // Add the income and reset expenses for each income
         setIncomes([...incomes, income]);
+        setExpenses([]);
     };
 
+
     return (
-        <div className="Dashboard">
+        <div className="App">
+            <Navbar/>
             <h1>Piggy</h1>
             <div className="tracker-container">
+                <Expense onAddExpense={addExpense} />
                 <Income onAddIncome={addIncome} />
             </div>
         </div>
     );
 }
-)}
 export default App;
