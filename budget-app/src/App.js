88f9b381244ffa69859c6e363b290b97fa5ed1f1
natprz42 <natprz42@gmail.com>
2024-06-30 12:23:48 +0200

    Add income logic

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,12 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Income from '../src/components/Income';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
-}
+    const [incomes, setIncomes] = useState([]);
+    const [totalIncome, setTotalIncome] = useState(0);
+    const [totalExpenditure, setTotalExpenditure] = useState(0);
+    const [remainingIncome, setRemainingIncome] = useState(0);
 
+    // Calculate total income
+    useEffect(() => {
+        let total = 0;
+        incomes.forEach(
+            (income) =>
+                (total += income.amount)
+        );
+        setTotalIncome(total);
+
+        // Calculate remaining income
+        setRemainingIncome(totalIncome - totalExpenditure);
+
+    const addIncome = (income) => {
+        // Add the income and reset expenses for each income
+        setIncomes([...incomes, income]);
+    };
+
+    return (
+        <div className="Dashboard">
+            <h1>Piggy</h1>
+            <div className="tracker-container">
+                <Income onAddIncome={addIncome} />
+            </div>
+        </div>
+    );
+}
+)}
 export default App;
