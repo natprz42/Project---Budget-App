8cf8a2a94b9234099649051d1f270044d05eb433
natprz42 <natprz42@gmail.com>
2024-07-11 13:28:28 +0200

    Error page

--- piggy-app/src/pages/Dashboard.jsx
+++ piggy-app/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 
 //  Local Storage
-import { fetchData } from "../LocalStorage.js"
+import { fetchData } from "../LocalStorage.jsx"
 
 export function dashboardLoader() {
     const userName = fetchData("userName");
